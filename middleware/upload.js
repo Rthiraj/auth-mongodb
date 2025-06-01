53ffcfaa978dfd94fb2ddb5951f231ad54de4b94
Rthiraj <aarthi.rp.17@gmail.com>
2025-06-01 10:48:11 +0530

    editeddddd

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,28 +1,28 @@
-// import multer from "multer";
-
-// const storage = multer.memoryStorage();
-
-// const upload = multer({
-//   storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, // 5MB max
-//   },
-// });
-
-// export default upload;
-
 import multer from "multer";
-import path from "path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // Make sure this folder exists
-  },
-  filename: function (req, file, cb) {
-    const uniqueName = Date.now() + "-" + file.originalname;
-    cb(null, uniqueName);
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB max
   },
 });
 
-const upload = multer({ storage });
 export default upload;
+
+// import multer from "multer";
+// import path from "path";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/"); // Make sure this folder exists
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueName = Date.now() + "-" + file.originalname;
+//     cb(null, uniqueName);
+//   },
+// });
+
+// const upload = multer({ storage });
+// export default upload;
