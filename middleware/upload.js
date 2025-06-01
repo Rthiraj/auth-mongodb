a9602f499ec70c5b185caed78104213a336b08ac
Rthiraj <aarthi.rp.17@gmail.com>
2025-06-01 11:07:02 +0530

    changesss

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,49 +1,49 @@
-// import multer from "multer";
-
-// const storage = multer.memoryStorage();
-
-// const upload = multer({
-//   storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024, // 5MB max
-//   },
-// });
-
-// export default upload;
-
-// middleware/upload.js
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
 
-// Get __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Ensure uploads directory exists
-const uploadDir = path.join(__dirname, "../uploads");
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB max
+  },
 });
 
 export default upload;
+
+// // middleware/upload.js
+// import multer from "multer";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import fs from "fs";
+
+// // Get __dirname in ES module
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // Ensure uploads directory exists
+// const uploadDir = path.join(__dirname, "../uploads");
+// if (!fs.existsSync(uploadDir)) {
+//   fs.mkdirSync(uploadDir, { recursive: true });
+// }
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     const ext = path.extname(file.originalname);
+//     cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+//   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+// });
+
+// export default upload;
 
 
 // import multer from "multer";
